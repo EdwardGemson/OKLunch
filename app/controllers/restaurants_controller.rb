05e6a7cd88061ef4f224067d1200e8b4c2ff1cd8
Edward <edwardgemson@gmail.com>
2018-03-13 18:25:46 -0400

    Update restaurants_controller.rb

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,6 @@
 class RestaurantsController < ApplicationController
 
-  #### THIS IS JUST SO I CAN GO TO THE CUSTOM ABOUT ####
-  #### INSTEAD OF THE LOGIN PAGE ####
-  #### KB : Also, delete these comments ####
+
   skip_before_action :ensure_current_user
 
   def index
